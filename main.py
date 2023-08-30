d19376587433ea1f066ad8e5d6e28e52dda57077
mshubhr <mshubhr@gmail.com>
2023-08-30 09:38:23 +0530

    Modifying with previous direction

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from spacecraft import execute_commands
 
 initial_position = (0, 0, 0)
 initial_direction = "N"
-commands = ["l", "f"]
+commands = ["f", "r", "u", "b", "l"]
 
 final_position, final_direction = execute_commands(initial_position, initial_direction, commands)
 
-print(final_position)
-print(final_direction)
+print("Final Position:", final_position)
+print("Final Direction:", final_direction)
